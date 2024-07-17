92da4b4ec0c908a216b9cbf15ed4bee512fc70f6
clairexue10 <xueyuheclaire@163.com>
2024-07-17 09:47:29 -0400

    change gpt to wc news

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='Wellesley News and Ethics of Technology',
+    # page_icon=''
 )
 
 # -----------------------------------------------------------------------------
@@ -22,10 +22,10 @@ def get_gdp_data():
     """
 
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    DATA_FILENAME = Path(__file__).parent/'gpted_wcnews_0714_cleaned_all_merged.csv'
+    raw_articles_df = pd.read_csv(DATA_FILENAME)
 
-    MIN_YEAR = 1960
+    MIN_YEAR = 2013
     MAX_YEAR = 2022
 
     # The data above has columns like:
